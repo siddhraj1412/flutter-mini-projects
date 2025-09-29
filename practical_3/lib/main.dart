9d2f648279385fefe4b3e9e501e11543230e8790
Siddhraj Thakor <149323330+siddhraj1412@users.noreply.github.com>
2025-09-30 04:02:31 +0530

    Add files via upload

---
+++ practical_3/lib/main.dart
@@ -0,0 +1,34 @@
+//PRACTICAL 3 : Multiscreen Application Login and dashboard and profile and
+//                Temperature convertor app
+// By 23AIML010 OM CHOKSI
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'dashboard.dart';
+import 'profile.dart';
+import 'todolist.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/todolist': (context) => TodoListScreen(),
+      },
+    );
+  }
+}
---
+++ practical_3/lib/profile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final userName = args?['name'] ?? 'User';
+    final userEmail = args?['email'] ?? 'No Email';
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('Name: $userName'), Text('Email: $userEmail')],
+        ),
+      ),
+    );
+  }
+}
